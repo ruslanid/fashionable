a8b28671eb2653b36a036d094197685f3b684686
unknown <ruslan.idrisov01@gmail.com>
2020-05-27 00:27:23 -0400

    Added loading indicator using HOC for collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,14 +3,18 @@ import {Switch, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
-
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.components';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import WithLoading from '../../components/with-loading/with-loading.component';
 
 import {fetchCollectionsAsync} from '../../redux/shop/shop.actions';
+
 import {
   selectAreCollectionsFetching, selectIsCollectionLoaded
 } from '../../redux/shop/shop.selectors';
+
+
+const CollectionPageWithLoading = WithLoading(CollectionPage);
 
 class ShopPage extends Component {
   componentDidMount() {
@@ -19,13 +23,13 @@ class ShopPage extends Component {
 
   render() {
     const {match, areCollectionsFetching, isCollectionLoaded} = this.props;
-
+    console.log(isCollectionLoaded);
     return (
       <div className="shop-page">
         <Switch>
           <Route
             path={`${match.path}/:collectionId`}
-            render={(props) => isCollectionLoaded ? (<CollectionPage {...props} />) : (<h2>Loading...</h2>)}
+            render={(props) => <CollectionPageWithLoading isLoading={!isCollectionLoaded} {...props} />}
           />
           <Route path={`${match.path}`} component={CollectionsOverview} />
         </Switch>
