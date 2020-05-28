0b83dba20264f170b707790697c4404ba8b250fe
unknown <ruslan.idrisov01@gmail.com>
2020-05-27 22:59:14 -0400

    Implemented persisting cart items in local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,17 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import directoryReducer from './directory/directory.reducer';
 import shopReducer from './shop/shop.reducer';
 import cartReducer from './cart/cart.reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['cart']
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -12,4 +20,4 @@ const rootReducer = combineReducers({
   cart: cartReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
